24eb1eab67f3a6b842e88eb961747284bc203d31
Sarah Specialist <sarah.specialist@gmail.com>
2022-04-27 11:45:01 +0800

    incorporating admin pages

--- orderapp/src/AdminApp.js
+++ orderapp/src/AdminApp.js
@@ -1,10 +1,10 @@
-import './App.css';
-import Header from './components/Header';
-import Home from './pages/Home';
-import Menu from './pages/Menu';
-import Orders from './pages/Orders';
-import Receipts from './pages/Receipts';
-import Payments from './pages/Payments';
+import './AdminApp.css';
+import Header from './adminComponents/Header';
+import Home from './adminPages/Home';
+import Menu from './adminPages/Menu';
+import Orders from './adminPages/Orders';
+import Receipts from './adminPages/Receipts';
+import Payments from './adminPages/Payments';
 import {BrowserRouter as Router, Switch, Route, Link, Redirect} from 'react-router-dom';
 
 function App() {
