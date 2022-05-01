a3f7c99b7a4fba098cb3814d465deeaab6925a98
Regina Yun <regina.yun.mei.lin@gmail.com>
2022-05-01 14:47:20 +0800

    added testing route for login

--- orderapp/src/LandingApp.js
+++ orderapp/src/LandingApp.js
@@ -2,6 +2,7 @@ import './LandingApp.css';
 import {BrowserRouter as Router, Switch, Route, Link, Redirect} from 'react-router-dom';
 import CustomerApp from './CustomerApp';
 import AdminApp from './AdminApp';
+import Login from './adminPages/login';
 
 function LandingApp() {
 
@@ -11,6 +12,7 @@ function LandingApp() {
      <Switch>  
           <Route path="/customer"><CustomerApp /></Route>
           <Route path="/admin"><AdminApp /></Route>
+          <Route path="/login"><Login /></Route>
     <div className="landing">
       <div className="main">
         <h1>Ambrosial</h1>
@@ -18,6 +20,7 @@ function LandingApp() {
         <div className="landing-nav"></div>
           <Link to="/customer" className="customer-nav">Customer</Link>
           <Link to="/admin" className="admin-nav">Admin</Link>
+          <Link to="/login" className="login-nav">Admin Login</Link>
         </div>
       </div>
     </Switch> 
