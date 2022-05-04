906f7a2537e37eecab711bac3d550b1dff987eb6
Regina Yun <regina.yun.mei.lin@gmail.com>
2022-05-05 02:42:08 +0800

    shifted landing app code to app js

--- orderapp/src/App.js
+++ orderapp/src/App.js
@@ -1,20 +1,30 @@
 import './App.css';
 import {BrowserRouter as Router, Switch, Route, Link, Redirect} from 'react-router-dom';
-import LandingApp from './LandingApp';
 import CustomerApp from './CustomerApp';
 import AdminApp from './AdminApp';
+import Login from './adminPages/login';
 
 function App() {
 
   return (
     <>
-    <Router>
-        <Switch>  
-          <Route exact path="/"><LandingApp /></Route>
+     <Router>
+     <Switch>  
           <Route path="/customer"><CustomerApp /></Route>
           <Route path="/admin"><AdminApp /></Route>
+          <Route path="/login"><Login /></Route>
+    <div className="landing">
+      <div className="main">
+        <h1 className='landing-title'>Ambrosial</h1>
+        <h2 className='landing-text'>I am a...</h2>
+        <div className="landing-nav"></div>
+          <Link to="/customer" className="customer-nav">Customer</Link>
+          <Link to="/admin" className="admin-nav">Admin</Link>
+          <Link to="/login" className="login-nav">Admin Login</Link>
           <Redirect from ="*" to="/" />
-        </Switch>
+        </div>
+      </div>
+    </Switch> 
     </Router>
     </>
   )
