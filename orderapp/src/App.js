347ee56bc090e8b6f4c00c428e619c6c58ed17ee
Regina Yun <regina.yun.mei.lin@gmail.com>
2022-05-05 02:52:55 +0800

    reverted app.js

--- orderapp/src/App.js
+++ orderapp/src/App.js
@@ -1,30 +1,20 @@
 import './App.css';
 import {BrowserRouter as Router, Switch, Route, Link, Redirect} from 'react-router-dom';
-import CustomerApp from './CustomerApp';
-import AdminApp from './AdminApp';
-import Login from './adminPages/login';
+import LandingApp from './LandingApp';
+import CustomerApp from './customer/CustomerApp';
+import AdminApp from './admin/AdminApp';
 
 function App() {
 
   return (
     <>
-     <Router>
-     <Switch>  
+    <Router>
+        <Switch>  
+          <Route exact path="/"><LandingApp /></Route>
           <Route path="/customer"><CustomerApp /></Route>
           <Route path="/admin"><AdminApp /></Route>
-          <Route path="/login"><Login /></Route>
-    <div className="landing">
-      <div className="main">
-        <h1 className='landing-title'>Ambrosial</h1>
-        <h2 className='landing-text'>I am a...</h2>
-        <div className="landing-nav"></div>
-          <Link to="/customer" className="customer-nav">Customer</Link>
-          <Link to="/admin" className="admin-nav">Admin</Link>
-          <Link to="/login" className="login-nav">Admin Login</Link>
           <Redirect from ="*" to="/" />
-        </div>
-      </div>
-    </Switch> 
+        </Switch>
     </Router>
     </>
   )
