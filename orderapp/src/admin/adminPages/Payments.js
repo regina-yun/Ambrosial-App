a8c7baafb6c3ac1f5dda525bc54a021814b25054
Regina Yun <regina.yun.mei.lin@gmail.com>
2022-05-06 23:11:13 +0800

    rough layout of payments.js

--- orderapp/src/admin/adminPages/Payments.js
+++ orderapp/src/admin/adminPages/Payments.js
@@ -1,9 +1,122 @@
+import { useEffect } from 'react';
+import { ambrosialAxiosAPI } from '../../api/api';
 import './Payments.css';
 
 export default function Payments() {
-    return (
-        <div className="payments">
-            This is the payments page
-        </div>
-    )
+
+	//Create Payment
+	async function createPayment(e) {
+		e.preventDefault();
+
+		await ambrosialAxiosAPI.post('/createpayment', {
+			receiptID: "",
+			paymentType: "",
+			paymentStatus: ""
+		})
+		.then((response) => {
+			console.log(`${response.config.method} method for route: ${response.config.url}`);
+			console.log(`response Status: ${response.data.status}`);
+			console.log(`response Message: ${response.data.message}`);
+			console.log("response Data: ", response.data.data);
+ 		})
+		.catch((error) => {
+				console.log(`${error.response.config.method} method for route: ${error.response.config.url}`);
+				console.log(`Error Status: ${error.response.data.status}`);
+				console.log(`Error Message: ${error.response.data.message}`);
+		});
+	}
+
+	//Update Payment
+	async function updatePayment(e) {
+		e.preventDefault();
+
+		await ambrosialAxiosAPI.put(`/updatepayment/${invoiceID}`, {
+			receiptID: "",
+			paymentType: "",
+			paymentStatus: ""
+		})
+		.then((response) => {
+			console.log(`${response.config.method} method for route: ${response.config.url}`);
+			console.log(`response Status: ${response.data.status}`);
+			console.log(`response Message: ${response.data.message}`);
+			console.log("response Data: ", response.data.data);
+ 		})
+		.catch((error) => {
+				console.log(`${error.response.config.method} method for route: ${error.response.config.url}`);
+				console.log(`Error Status: ${error.response.data.status}`);
+				console.log(`Error Message: ${error.response.data.message}`);
+		});
+	}
+
+	//Delete Payment
+	async function deletePayment(e) {
+		e.preventDefault();
+
+		await ambrosialAxiosAPI.delete(`/deletepayment/${invoiceID}`)
+		.then((response) => {
+			console.log(`${response.config.method} method for route: ${response.config.url}`);
+			console.log(`response Status: ${response.data.status}`);
+			console.log(`response Message: ${response.data.message}`);
+			console.log("response Data: ", response.data.data);
+ 		})
+		.catch((error) => {
+				console.log(`${error.response.config.method} method for route: ${error.response.config.url}`);
+				console.log(`Error Status: ${error.response.data.status}`);
+				console.log(`Error Message: ${error.response.data.message}`);
+		});
+	}
+
+	//Read All Payment
+	useEffect(() => {
+		getAllPayment();
+	});
+	
+	async function getAllPayment(e) {
+		e.preventDefault();
+
+		await ambrosialAxiosAPI.get('/viewpaymentlogs')
+		.then((response) => {
+			console.log(`${response.config.method} method for route: ${response.config.url}`);
+			console.log(`response Status: ${response.data.status}`);
+			console.log(`response Message: ${response.data.message}`);
+			console.log("response Data: ", response.data.data);
+ 		})
+		.catch((error) => {
+				console.log(`${error.response.config.method} method for route: ${error.response.config.url}`);
+				console.log(`Error Status: ${error.response.data.status}`);
+				console.log(`Error Message: ${error.response.data.message}`);
+		});
+	}
+
+	//Read Specific Payment
+	async function getSpecificPayment(e) {
+		e.preventDefault();
+
+		await ambrosialAxiosAPI.get(`/viewpaymentlogs/${invoiceID}`)
+		.then((response) => {
+			console.log(`${response.config.method} method for route: ${response.config.url}`);
+			console.log(`response Status: ${response.data.status}`);
+			console.log(`response Message: ${response.data.message}`);
+			console.log("response Data: ", response.data.data);
+ 		})
+		.catch((error) => {
+				console.log(`${error.response.config.method} method for route: ${error.response.config.url}`);
+				console.log(`Error Status: ${error.response.data.status}`);
+				console.log(`Error Message: ${error.response.data.message}`);
+		});
+	}
+
+	return (
+		<div className="payments">
+				<h1>Payment Logs</h1>
+				<table>
+					<tr>
+						<th>No.</th>
+						<th>Receipt ID</th>
+						<th>Payment Type</th>
+						<th>Payment Status</th>
+					</tr>
+				</table>
+		</div>
+	)
 }
