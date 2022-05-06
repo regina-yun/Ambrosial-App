c458597d38aba620eae816430f1c2fd35826eaa9
Regina Yun <regina.yun.mei.lin@gmail.com>
2022-05-07 01:41:34 +0800

    set event handlers for create payment

--- orderapp/src/admin/adminPages/Payments.js
+++ orderapp/src/admin/adminPages/Payments.js
@@ -6,9 +6,43 @@ import './Payments.css';
 export default function Payments() {
 
 	//Create Payment
+	const [paymentInput, setPaymentInput] = useState({receiptID: 0, paymentType: '', paymentStatus: ''});
+
+	const [createPaymentMessage, setCreatePaymentMessage] = useState('');
+	const [createPaymentMessageStatus, setCreatePaymentMessageStatus] = useState(false);
 
 	const [modalVisibleCreatePayment, setModalVisibleCreatePayment] = useState(false);
 	const [createPaymentPopupOpen, setCreatePaymentPopupOpen] = useState(false);
+	const [modalVisibleCreatePaymentConfirmation, setModalVisibleCreatePaymentConfirmation] = useState(false);
+	const [createPaymentConfirmationPopupOpen, setCreatePaymentConfirmationPopupOpen] = useState(false);
+
+	function handleCreatePaymentOnChange(e) {
+		let createPaymentInput = {...paymentInput};
+		createPaymentInput[e.target.name] = e.target.value;
+
+		setPaymentInput(createPaymentInput);
+	}
+
+	function onSubmitCreatePaymentInputValidation(e) {
+		e.preventDefault();
+
+		if(!paymentInput.receiptID && !paymentInput.paymentType && !paymentInput.paymentStatus) {
+			setCreatePaymentMessageStatus(true)
+			setCreatePaymentMessage('***Please check the input fields***');
+			return;
+		}
+		else if(!paymentInput.receiptID || !paymentInput.paymentType || !paymentInput.paymentStatus) {
+			setCreatePaymentMessageStatus(true)
+			setCreatePaymentMessage('***Please check the input fields***');
+			return;
+		}
+	}
+
+	function togglePopupCreatePayment() {
+		setModalVisibleCreatePayment(!modalVisibleCreatePayment);
+		setCreatePaymentPopupOpen(!createPaymentPopupOpen);
+	}
+
 
 	async function createPayment(e) {
 		e.preventDefault();
@@ -112,56 +146,75 @@ export default function Payments() {
 		});
 	}
 
-	function togglePopupCreatePayment() {
-		setModalVisibleCreatePayment(!modalVisibleCreatePayment);
-		setCreatePaymentPopupOpen(!createPaymentPopupOpen);
-	}
+
 
 	return (
 		<>
 			<div className='createAndRefresh'>
-				<button className='refreshPaymentLogs' onClick={e => getAllPayment(e)}>Refresh List</button>
+				<button className='refreshPaymentLogs' onClick={getAllPayment}>Refresh List</button>
 				<button className='createPayment' onClick={togglePopupCreatePayment}>Create New Payment Log</button>
 			</div>
 
 
 			{/* insert modal code here */}
 
-			{modalVisibleCreatePayment && <div className='create-payment-modal'>
-				{createPaymentPopupOpen && <Popup
-					popupType='createPaymentPopup'
-					handleClose={togglePopupCreatePayment}
-					content={
-						<form>
-							<label className='formHeader'>Create New Payment Log</label>
+			{modalVisibleCreatePayment && <div className='modal'>
+			{createPaymentPopupOpen && <Popup
+				popupType='createPaymentPopup'
+				handleClose={togglePopupCreatePayment}
+				content={
+					<form onSubmit={onSubmitCreatePaymentInputValidation}>
+						<label className='formHeader'>Create New Payment Log</label>
 
-							<label className='formLabelText'>Receipt ID</label>
-							<input className='createPaymentInputReceiptId' type='number' />
-							
-							<label className='formLabelText'>Payment Type</label>
-							<input className='createPaymentInputPaymentType' type='text'/>
+						<label className='formLabelText'>Receipt ID</label>
+						<input className='createPaymentInputReceiptId' type='number' name='receiptID' onChange={handleCreatePaymentOnChange}/>
+						
+						<label className='formLabelText'>Payment Type</label>
+						<input className='createPaymentInputPaymentType' type='text' name='paymentType' onChange={handleCreatePaymentOnChange}/>
 
-							<label className='formLabelText'>Payment Status</label>
-							<input className='createPaymentInputPaymentStatus' type='text'/>
-							
-							<button className='createPaymentButton'>Submit</button>
-						</form>
-					}/>
-				}
-				</div>
+						<label className='formLabelText'>Payment Status</label>
+						<input className='createPaymentInputPaymentStatus' type='text' name='paymentStatus' onChange={handleCreatePaymentOnChange}/>
+						
+						<button className='createPaymentButton'>Submit</button>
+
+						{createPaymentMessageStatus && <label className='formLabelTextStatus'>{<label className='formLabelText'>{createPaymentMessage}</label>}</label>}
+					</form>
+				}/>
 			}
+			</div>
+			}
+
+			{modalVisibleCreatePaymentConfirmation && <div className='modal'></div>}
+				{createPaymentConfirmationPopupOpen && <Popup
+				popupType='deleteReceiptConfirmationPopup'
+				handleClose={togglePopupDeleteReceiptConfirmation}
+				content={
+						<div>
+								<label className='deleteReceiptConfirmationHeader'>Are You Sure ?</label>
+								<br></br>
+								{!postDataClicked ? 
+										<div>
+												<button className='deleteReceiptConfirmationYesButton' onClick={updateReceipt}>Yes</button>
+												<button className='deleteReceiptConfirmationNoButton' onClick={closePopupUpdateReceiptConfirmation}>No</button>
+										</div>:
+										<button type="button" className='deleteReceiptConfirmationYesButton'  onClick={handleClosePopups} >Close</button>
+								}
+								<br></br>
+								{postDataClicked ? <div className='deleteReceiptConfirmationStatusMessageContainer'><label className='deleteReceiptConfirmationStatusMessage'>{postStatusMessage}</label></div>: null}
+						</div>
+				}/>}   
 				
 
 			<div className="payments">
 					<h1>Payment Logs</h1>
 					<table>
-						<thead>
+						<tr>
 							<th>No.</th>
 							<th>Receipt ID</th>
 							<th>Payment Type</th>
 							<th>Payment Status</th>
 							<th colSpan='2'>Actions</th>
-						</thead>
+						</tr>
 
 						{allPaymentLogsData.map((paymentLogs, index) => {
 							<tr key={paymentLogs.receiptID}>
