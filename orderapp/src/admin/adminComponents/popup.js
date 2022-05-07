3585aa5d0820841272b7c6e2a525cdb22c7fb9d9
Regina Yun <regina.yun.mei.lin@gmail.com>
2022-05-07 11:24:04 +0800

    added delete payment popup skeleton

--- orderapp/src/admin/adminComponents/popup.js
+++ orderapp/src/admin/adminComponents/popup.js
@@ -172,6 +172,26 @@ function Popup(props){
             </div>
         );
     }
+    else if(props.popupType === 'deletePaymentPopup'){
+        return(
+            <div className="deletePaymentPopup-box">
+                <div className="deletePayment-box">
+                    <span className="deletePayment-close-icon" onClick={props.handleClose}>x</span>
+                        {props.content}
+                </div>
+            </div>
+        );
+    }
+    else if(props.popupType === 'deletePaymentConfirmationPopup'){
+        return(
+            <div className="deletePaymentConfirmationPopup-box">
+                <div className="deletePaymentConfirmation-box">
+                    <span className="deletePaymentConfirmation-close-icon" onClick={props.handleClose}>x</span>
+                        {props.content}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Popup;
