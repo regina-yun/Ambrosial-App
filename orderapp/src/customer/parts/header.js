803a19643a1c4c1169de64d68e2295e1c714bf12
Regina Yun <regina.yun.mei.lin@gmail.com>
2022-05-09 20:06:21 +0800

    updated logos

--- orderapp/src/customer/parts/header.js
+++ orderapp/src/customer/parts/header.js
@@ -1,11 +1,13 @@
 import './header.css';
+import LoginHeader from '../../admin/adminComponents/login-header';
 
 function Header() {
     return (
-      <div className="header-title">
-            Ambrosial <br/>
-          <p className='header-subtitle'>quality you can taste</p>
-      </div>
+      // <div className="header-title">
+      //       Ambrosial <br/>
+      //     <p className='header-subtitle'>quality you can taste</p>
+      // </div>
+      <LoginHeader />
     );
   }
   
